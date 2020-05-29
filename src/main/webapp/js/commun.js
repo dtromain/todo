9ee1a69e32785898bb9cc9734e2fa221defebb6f
Romain Douteau <personnel@romaindouteau.fr>
2020-05-29 15:19:45 +0200

    Fix

--- src/main/webapp/js/commun.js
+++ src/main/webapp/js/commun.js
@@ -1,13 +1,13 @@
 $(function(){
-	
-	$(".dt").on("click", function(){
+
+	$('[id^="dt_"]').on("click", function(){
 		var att = $(this).attr("id");
 		var rep = confirm("Do you really want to delete this task ?");
 		if (rep)
 			window.location="deleteTask?index=" + att;
 	});
-	
-	$(".ut").on("click", function(){
+
+	$('[id^="ut_"]').on("click", function(){
 		var att = $(this).attr("id");
 		window.location="updateTask?index=" + att;
 	});
